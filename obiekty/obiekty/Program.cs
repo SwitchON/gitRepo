d2e6d8cbb709d55aad8e8a174995ba55f1a855d4
Adam <p41@op.pl>
2012-10-15 23:51:00 +0200

    init

---
+++ obiekty/obiekty/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace obiekty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A");
+            Console.ReadKey();
+        }
+    }
+}
